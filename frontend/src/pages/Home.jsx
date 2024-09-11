b638b3d89167812b057487c7674d5e1aaf5a7b00
erick-santos-8 <esagosto@hotmail.com>
2024-09-11 17:06:21 -0300

    home fetch

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,49 @@
-import {Box, Flex} from "@chakra-ui/react"
+import {Box, Flex, useToast} from "@chakra-ui/react"
 import Search from "../components/Search";
 import SortRepos from "../components/SortRepos"
 import ProfileInfo from "../components/ProfileInfo"
 import Repos from "../components/Repos"
+import { useCallback, useEffect, useState } from "react";
+
 
 const Home = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState("recents");
+
+  const toast = useToast();
+  const getProfileAndRepos = useCallback(async () => {
+    setLoading(true)
+    try {
+      const res = await fetch("https://api.github.com/users/erick-santos-8")
+      const data = await res.json();
+      setUserProfile(data);
+      
+      
+      const resRepos = await fetch(userProfile.repos_url);
+      const dataRepos = await resRepos.json();
+      setRepos(dataRepos);
+
+      console.log("user", userProfile)
+      console.log("repos", repos);
+      
+    } catch (error) {
+      toast({
+        title: 'Erro',
+        description: error.message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      })
+    }finally{
+      setLoading(false);
+    }
+  },[])
+
+  useEffect(()=>{
+    getProfileAndRepos();
+  },[getProfileAndRepos])
   return (
     <Box m={"4"}>
       <Search/>
